35ce4bd569648f58f8922a7e376bcf1461db2a79
diego_neri <diego@diegoneri.com>
2020-06-17 23:17:42 -0300

    Finaliza programa Senha

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Senha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string senha = "1234abcd";
+            string senhaInformada;
+
+            Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+            senhaInformada = Console.ReadLine();
+
+            if (senha.Equals(senhaInformada)){
+                Console.WriteLine("Acesso permitido");
+            }else{
+                Console.WriteLine("Acesso negado");
+            }
         }
     }
 }
